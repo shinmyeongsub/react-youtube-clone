98f365998c777ca0cd6bc39fda82df15a978b9b8
shinmyeongsub <joyful2529@naver.com>
2021-10-25 09:34:54 +0900

    2021/10/25 파일 업로드

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,11 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Typography, Button, Form, message, Input, Icon} from 'antd';
 import Dropzone from 'react-dropzone';
+import Axios from 'axios';
 
 const {TextArea} = Input;
 const {Title} = Typography;
 
+const PrivateOptions = [
+    {value : 0, label : "Private"},
+    {value : 1, label : "public"}
+
+]
+
+const CategoryOptions = [
+    { value : 0, label : "Filem & Animation"},
+    { value : 1, label : "Autos & Vehicles"},
+    { value : 2, label : "Music"},
+    { value : 3, label : "Pets & Animals"},
+]
+
+
 function VideoUploadPage() {
+
+    const [VideoTitle  , setVideoTitle] = useState("")
+    const [Description, setDescription] = useState("")
+    const [Private, setPrivate] = useState(0)
+    const [Category, setCategory] = useState("Film & Animation")
+
+    const onTitleChange = (e) =>{
+        setVideoTitle(e.currentTarget.value)
+    }
+
+    const onDescriptionChange = (e) =>{
+        setDescription(e.currentTarget.value)
+    }
+
+    const onPrivateChange = (e) =>{
+        setPrivate(e.currentTarget.value)
+    }
+
+    const onCategoryChange = (e) =>{
+        setCategory(e.currentTarget.value)
+    }
+
+    const onDrop = (files) =>{
+        let formData = new FormData;
+        const config = {
+            header : {'content-type' : 'multipart/form-data'}
+        }
+        formData.append("file",files[0])
+
+        Axios.post('/api/video/uploadfiles',formData, config)
+        .then(response => {
+            if(response.data.success){
+                console.log(response.data)
+            } else {
+                alert('비디오 업로드 실패')
+            }
+        })
+    }
+
     return (
         <div style={{maxWidth:'700px', margin:'2rm auto'}}>
                 <div style={{textAlign:'center',marginBottom:'2rem'}}>
@@ -16,9 +70,10 @@ function VideoUploadPage() {
                     <div style={{display:'flex', justifyContent:'space-between'}}>
                         {/* Drop zone */}
                         <Dropzone
-                        onDrop
-                        multiple
-                        maxSize>
+                        onDrop= {onDrop}
+                        multiple = {false}
+                        maxSize = {1000000000}
+                        >
                             {({ getRootProps, getInputProps}) => (
                                 <div style ={{ width: '300px', height : '240px', border:'1px solid lightgray', alignItems:'center', justifyContent:'center'}} {...getRootProps()}>
                                     <input {...getInputProps()} />
@@ -38,26 +93,30 @@ function VideoUploadPage() {
                 <br />
                 <label>Title</label>
                 <Input
-                    onChange
-                    value
+                    onChange = {onTitleChange}
+                    value = {VideoTitle}
                 />
                 <br/>
                 <br/>
                 <label>Description</label>
                 <TextArea
-                    onChange
-                    value
+                    onChange = {onDescriptionChange}
+                    value = {Description}
                 />
 
                 <br/>
                 <br/>
 
-                <select onChange>
-                    <option key value></option>
+                <select onChange = {onPrivateChange}>
+                    {PrivateOptions.map((item, index) => (
+                        <option key = {index} value = {item.value}>{item.label}</option>
+                    ))}
                 </select>
 
-                <select onChange>
-                    <option key value></option>
+                <select onChange = {onCategoryChange}>
+                    {CategoryOptions.map((item, index)=>(
+                        <option key = {index} value ={item.value}>{item.label}</option>
+                    ))}
                 </select>
 
                 <Button type="primary" size="large" onClick>
