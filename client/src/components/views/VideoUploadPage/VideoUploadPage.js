3770cf98937b371471f7120f3bb142a6291d4bb8
shinmyeongsub <joyful2529@naver.com>
2021-10-26 08:55:50 +0900

    2021/10/26 썸네일 업로드

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -26,6 +26,9 @@ function VideoUploadPage() {
     const [Description, setDescription] = useState("")
     const [Private, setPrivate] = useState(0)
     const [Category, setCategory] = useState("Film & Animation")
+    const [FilePath, setFilepath] = useState("");
+    const [Duration, setDuration] = useState("");
+    const [ThumbnailPath, setThumbnailPath] = useState("");
 
     const onTitleChange = (e) =>{
         setVideoTitle(e.currentTarget.value)
@@ -54,6 +57,27 @@ function VideoUploadPage() {
         .then(response => {
             if(response.data.success){
                 console.log(response.data)
+
+                let variable = {
+                    url : response.data.url,
+                    fileName : response.data.fileName
+                }
+
+                setFilepath(response.data.url);
+
+                Axios.post('/api/video/thumbnail', variable)
+                .then(response => {
+                    if(response.data.success){
+                        console.log(response.data);
+
+                        setDuration(response.data.fileDuration)
+                        setThumbnailPath(response.data.url)
+
+                    }else{
+                        alert('썸네일 생성에 실패 했습니다.');
+                    }
+                })
+
             } else {
                 alert('비디오 업로드 실패')
             }
@@ -83,9 +107,12 @@ function VideoUploadPage() {
 
                         </Dropzone>
                         {/* Tjumbnail */}
-                        <div>
-                            <img src alt />
-                        </div>
+                        {ThumbnailPath &&
+                            <div>
+                                <img src={`http://localhost:5000/${ThumbnailPath}`} alt="thumbnail" />
+                            </div>
+                        }
+
                     </div>
 
 
