f2ab2c8b93f293307cc7f216778f70423f17facf
shinmyeongsub <joyful2529@naver.com>
2021-10-27 10:03:06 +0900

    2021/10/27 Video upload , landing page

--- server/models/User.js
+++ server/models/User.js
@@ -1,93 +1,41 @@
+const { Schema } = require('mongoose');
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const saltRounds = 10;
-const jwt = require('jsonwebtoken');
-const moment = require("moment");
 
-const userSchema = mongoose.Schema({
-    name: {
+
+const videoSchema = mongoose.Schema({
+
+    writer:{
+        type : Schema.Type.ObjectId,
+        ref:'User'
+    },
+    title : {
         type:String,
-        maxlength:50
+        maxLength:50
     },
-    email: {
-        type:String,
-        trim:true,
-        unique: 1 
+    description :{
+        type : String
     },
-    password: {
-        type: String,
-        minglength: 5
+    privacy : {
+        type : Number
     },
-    lastname: {
-        type:String,
-        maxlength: 50
+    filePath : {
+        type :String
     },
-    role : {
-        type:Number,
-        default: 0 
+    category : {
+        type:String
     },
-    image: String,
-    token : {
-        type: String,
+    views: {
+        type : Number,
+        default : 0
     },
-    tokenExp :{
-        type: Number
+    duration : {
+        type : String
+    },
+    thumbnail : {
+        type : String
     }
-})
+},{timestamps : true})
 
+const Video = mongoose.model('Video', videoSchema);
 
-userSchema.pre('save', function( next ) {
-    var user = this;
-    
-    if(user.isModified('password')){    
-        // console.log('password changed')
-        bcrypt.genSalt(saltRounds, function(err, salt){
-            if(err) return next(err);
-    
-            bcrypt.hash(user.password, salt, function(err, hash){
-                if(err) return next(err);
-                user.password = hash 
-                next()
-            })
-        })
-    } else {
-        next()
-    }
-});
-
-userSchema.methods.comparePassword = function(plainPassword,cb){
-    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
-        if (err) return cb(err);
-        cb(null, isMatch)
-    })
-}
-
-userSchema.methods.generateToken = function(cb) {
-    var user = this;
-    console.log('user',user)
-    console.log('userSchema', userSchema)
-    var token =  jwt.sign(user._id.toHexString(),'secret')
-    var oneHour = moment().add(1, 'hour').valueOf();
-
-    user.tokenExp = oneHour;
-    user.token = token;
-    user.save(function (err, user){
-        if(err) return cb(err)
-        cb(null, user);
-    })
-}
-
-userSchema.statics.findByToken = function (token, cb) {
-    var user = this;
-
-    jwt.verify(token,'secret',function(err, decode){
-        user.findOne({"_id":decode, "token":token}, function(err, user){
-            if(err) return cb(err);
-            cb(null, user);
-        })
-    })
-}
-
-const User = mongoose.model('User', userSchema);
-
-module.exports = { User }
+module.exports = { Video }
--- server/routes/video.js
+++ server/routes/video.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-// const { Video } = require("../models/Video");
+const { Video } = require("../models/Video");
 
 const { auth } = require("../middleware/auth");
 const multer = require("multer");
@@ -38,6 +38,32 @@ router.post('/uploadfiles',(req, res) =>{
         }
         return res.json({success:true, url: res.req.file.path, fileName : res.req.file.filename})
     })
+
+})
+
+router.post('/uploadVideo',(req, res) =>{
+
+    // 비디오 정보들을 저장한다.
+
+   const video =  new Video(req.body)
+
+   video.save((err,doc)=>{
+       if(err) return res.json({success : false, err})
+       res.status(200).json({success : true})
+   })
+
+})
+
+router.post('/getVideos',(req, res) =>{
+
+    // 비디오를 DB에서 가져와서 클라이언트에 보낸다.
+    
+    Video.find()
+        .populate('writer')
+        .exec((err,videos)=>{
+            if(err) return express.status(400).send(err);
+            res.status(200).json({success:true, videos})
+        })
 
 })
 
